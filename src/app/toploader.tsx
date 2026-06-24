0a28bbe8cd4ecbb598ea2506da1c6f7e82f58ee3
mohit-rajput-raj <rajputmohit19101169@gmail.com>
2026-06-24 20:26:01 +0530

    updates-and-ui-fix-2406

--- src/app/toploader.tsx
+++ src/app/toploader.tsx
@@ -7,7 +7,7 @@ type Props = {}
 const TopLoader = (props: Props) => {
     return (
         <NextTopLoader
-            color="#FF0000"
+            color="#067529ff"
             initialPosition={0.08}
             crawlSpeed={200}
             height={3}
@@ -15,7 +15,7 @@ const TopLoader = (props: Props) => {
             showSpinner={false}
             easing="ease"
             speed={500}
-            shadow="0 0 10px #FF0000,0 0 5px #FF0000"
+            shadow="0 0 10px #21d469ff,0 0 5px #31ba58ff"
         />
     )
 }
